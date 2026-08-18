c39ea60b0052e7dd12182a425d781b469ae0d561
ArkanaDeck <arkana.tarot.market@gmail.com>
2026-08-18 17:12:50 +0100

    fix: add style sheet import path layout

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import './main-layout.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-
 const container = document.getElementById('app');
 
 if (!container) {
