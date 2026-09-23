a0489c410dde593a453fbd088abd80c5ff1f9f2d
ArkanaDeck <arkana.tarot.market@gmail.com>
2026-09-23 20:19:18 +0100

    site map push the file

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import Sitemap from 'vite-plugin-sitemap';
+
+const coreRoutes = ['/', '/search', '/account', '/help'];
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://arkcards.com',
+      dynamicRoutes: coreRoutes.filter((route) => route !== '/'),
+    }),
+  ],
   build: {
     outDir: 'dist',
     sourcemap: false,
